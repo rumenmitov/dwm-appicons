da9afcf19aba17245bb9f968f5f3da392afc3b00
Rumen <rumenmitov@protonmail.com>
2025-01-04 21:34:15 +0100

    appicons patch
    Adds support for dynamic icons instead of tags. Tag icons are updated
    whenever client is spawned or closed.

--- config.def.h
+++ config.def.h
@@ -21,14 +21,20 @@ static const char *colors[][3]      = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
+/* appicons */
+/* NOTE: set to 0 to set to default (whitespace) */
+static char outer_separator_beg = '[';
+static char outer_separator_end = ']';
+static char inner_separator     = ' ';
+
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      instance    title       tags mask     isfloating   monitor   appicon*/
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1,         NULL },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1,         "󰈹"  },
 };
 
 /* layout(s) */
